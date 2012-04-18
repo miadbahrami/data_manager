e3b419aa9b5848c9bd8363dd575ef1e69173641d
Amin Oruji <aminpy@gmail.com>
2012-04-18 11:30:31 +0430

    some progress in project

--- export_data.py
+++ export_data.py
@@ -1,33 +1,34 @@
 from osv import osv, fields
+import cStringIO
+import base64
 
 
 class ExportDataWizard(osv.osv_memory):
     _name = 'export.data.wizard'
     _inherit = 'ir.wizard.screen'
 
+    def export_data(self, cr, uid, ids, context=None):
+        this = self.browse(cr, uid, ids, context=context)[0]
+        this.name = '%s.json' % this.export_data_wizard_class_name
+        buf = cStringIO.StringIO()
+        buf.write("durud bar to bad")
+        out = base64.encodestring(buf.getvalue())
+        buf.close()
+        return self.write(cr, uid, ids, {
+            'state': 'done', 'export_data_wizard_data': out,
+            'name': this.name
+        }, context=context)
+
     _columns = {
+        'name': fields.char('Filename', size=16, readonly=True),
         'export_data_wizard_class_name': fields.char(
             'Export Data Wizard Class Name', size=64),
-        'export_data_wizard_data': fields.binary('Export Data Wizard Data'),
+        'export_data_wizard_data': fields.binary('Export Data Wizard Data',
+                                                 readonly=True),
         'state': fields.selection([('init', 'init'), ('done', 'done')],
                                   'state', readonly=True),
     }
 
     _defaults = {'state': 'init'}
 
-    def export_data(self, cr, uid, ids, context=None):
-        print "hello world"
-
-        self.write(cr, uid, ids, {
-            'state': 'done', 'export_data_wizard_data': 'durud'
-        }, context=context)
-
-        return {
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'export.data.wizard',
-            'type': 'ir.actions.act_window',
-            'target': 'new',
-            'res_id': ids and ids[0] or False,
-        }
 ExportDataWizard()
