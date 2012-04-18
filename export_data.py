231c7d0d5505053dfd000370719a7be97af49821
Amin Oruji <aminpy@gmail.com>
2012-04-19 01:13:31 +0430

    some progressing in logic

--- export_data.py
+++ export_data.py
@@ -7,9 +7,31 @@ class ExportDataWizard(osv.osv_memory):
     _inherit = 'ir.wizard.screen'
 
     def export_data(self, cr, uid, ids, context=None):
+        generated_content = []
+        generated_dict = {}
+        generated_fields = {}
         this = self.browse(cr, uid, ids, context=context)[0]
-        this.name = '%s.json' % this.export_data_wizard_class_name
-        out = base64.encodestring("durud bar to sds")
+        model_name = this.ir_model_id.model
+        model_field_list = this.ir_model_id.field_id
+
+        for model_field in model_field_list:
+            if model_field.ttype in ['char', 'integer', 'many2one', 'float', 'date', 'time', 'datetime']:
+                generated_fields[model_field.name] = None
+
+        current_obj = self.pool.get(model_name)
+        current_obj_id_list = current_obj.search(cr, uid, [], context=context)
+        current_list = current_obj.browse(cr, uid, current_obj_id_list, context=context)
+        this.name = "%s.py" % model_name
+
+        for current in current_list:
+            generated_dict['pk'] = current.id
+            generated_dict['model'] = model_name
+            generated_dict['fields'] = generated_fields
+
+        generated_content.append(generated_dict)
+
+        out = base64.encodestring(str(generated_content))
+
         return self.write(cr, uid, ids, {
             'state': 'done', 'export_data_wizard_data': out,
             'name': this.name
