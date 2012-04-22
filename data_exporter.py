57748206aec8679de03fc0125f51cfe260dbac8b
Amin Oruji <aminpy@gmail.com>
2012-04-22 15:32:01 +0430

    iso_code mandatory and duplication warning added for main class

--- data_exporter.py
+++ data_exporter.py
@@ -10,7 +10,8 @@ class DeExportDataWizard(osv.osv_memory):
 
     def export_data(self, cr, uid, ids, context=None):
         this = self.browse(cr, uid, ids, context=context)[0]
-        model_name = this.ir_model_id.model
+        ir_model_id = this.ir_model_id
+        model_name = ir_model_id.model
 
         input_model_obj = self.pool.get(model_name)
         input_model_id_list = input_model_obj.search(cr, uid, [],
@@ -21,6 +22,21 @@ class DeExportDataWizard(osv.osv_memory):
 
         input_model_list = input_model_obj.read(cr, uid, input_model_id_list,
                                                 [], context=context)
+        # Checking for iso_code field for main class
+        if 'iso_code' not in input_model_list[0]:
+            raise osv.except_osv((_('Warning')),
+                                 (_("%s class does not have iso_code field" % model_name)))
+
+        # Checking for duplication of iso_code content for main class
+        iso_code_list = []
+        for input_model in input_model_list:
+            iso_code_list.append(input_model['iso_code'])
+
+        if len(iso_code_list) > len(set(iso_code_list)):
+            raise osv.except_osv(
+                (_('Warning')),
+                (_("iso_code field values of %s class must be unique" %
+                   model_name)))
 
         # Creating XML
         openerp_tag = etree.Element('openerp')
@@ -33,10 +49,11 @@ class DeExportDataWizard(osv.osv_memory):
                                                  input_model['id'])
             record_tag.attrib['model'] = model_name
 
-            for ir_model_field in this.ir_model_id.field_id:
+            for ir_model_field in ir_model_id.field_id:
                 field_tag = etree.SubElement(record_tag, 'field')
                 field_tag.attrib['name'] = ir_model_field.name
 
+                # data exporter doesn't support many2many field right now
                 if ir_model_field.ttype not in ['one2many', 'many2many']:
                     if ir_model_field.ttype == 'many2one':
                         temp_object_obj = self.pool.get(ir_model_field.model)
@@ -49,10 +66,13 @@ class DeExportDataWizard(osv.osv_memory):
                                                                  context=context)
                             temp_object_rel_name = temp_object._model._columns[ir_model_field.name]._obj
                             temp_object_rel_obj = self.pool.get(temp_object_rel_name)
-                            # Checking for Duplication
+
+                            ### Checking for Duplication of many2one field class iso_code content
                             iso_code_list = []
                             temp_object_rel_id_list = temp_object_rel_obj.search(cr, uid, [], context=context)
-                            temp_object_rel_list = temp_object_rel_obj.browse(cr, uid, temp_object_rel_id_list, context=context)
+                            temp_object_rel_list = temp_object_rel_obj.browse(
+                                cr, uid, temp_object_rel_id_list,
+                                context=context)
 
                             for temp_object_rel in temp_object_rel_list:
                                 iso_code_list.append(temp_object_rel.iso_code)
@@ -61,7 +81,9 @@ class DeExportDataWizard(osv.osv_memory):
                                 raise osv.except_osv(
                                     (_('Warning')),
                                     (_("iso_code field values of %s class must be unique" % ir_model_field.model)))
+                            ###
 
+                            ### check iso_code of many2one field class
                             temp_object_rel = temp_object_rel_obj.browse(
                                 cr, uid, input_model[ir_model_field.name][0])
 
@@ -73,6 +95,7 @@ class DeExportDataWizard(osv.osv_memory):
                                     (_('Warning')),
                                     (_("%s class doesn't have iso_code field!" %
                                        ir_model_field.model)))
+                            ###
 
                             field_tag.attrib['search'] = "[('iso_code', '=', %s)]" % temp_iso_code
                             field_tag.attrib['model'] = temp_object_rel_name
