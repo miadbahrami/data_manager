92f147bc010b668195ffa03c315769f6c44dfaa6
Amin Oruji <aminpy@gmail.com>
2012-04-30 19:46:18 +0430

    some huge changes for add model or module choice feature

--- data_exporter.py
+++ data_exporter.py
@@ -15,50 +15,65 @@ class DmExportDataWizard(osv.osv_memory):
         ir_model_id_input = this.ir_model_id
         ir_module_module_input = this.ir_module_module_id
 
-        model_name = ir_model_id_input.model
-
-        ### Getting Export model
-        input_model_obj = self.pool.get(model_name)
-        input_model_id_list = input_model_obj.search(cr, uid, [],
-                                                     context=context)
-        # Checking for Empty data
-        if not input_model_id_list:
-            raise osv.except_osv((_('Warning')),
-                ('There is no data for %s class' % model_name))
-
-        # Select * from ...
-        input_model_list = input_model_obj.read(cr, uid, input_model_id_list,
-                                                [], context=context)
-        ###
-
         data_json = []
 
-        # Iterating export model data
-        for input_model in input_model_list:
-            record_dict = {}
-            field_dict = {}
-            record_dict['pk'] = input_model['id']
-            record_dict['model'] = model_name
+        ir_model_list = []
 
-            # Iterating model fields
-            for ir_model_field in ir_model_id_input.field_id:
-                # Checking for existing model name in input model
-                if ir_model_field.name in input_model:
-                    if ir_model_field.ttype == 'many2one':
-                        field_dict[ir_model_field.name] = \
-                            input_model[ir_model_field.name][0]
-                    else:
-                        field_dict[ir_model_field.name] = \
-                            input_model[ir_model_field.name]
+        # Check for based on module
+        if export_type_input == 'b':
+            ir_model_obj = self.pool.get('ir.model')
+            ir_model_id_list = ir_model_obj.search(cr, uid,
+                [('modules', '=', ir_module_module_input.name)],
+                context=context)
+            ir_model_list = ir_model_obj.browse(cr, uid, ir_model_id_list,
+                                                context=context)
 
-            record_dict['fields'] = field_dict
-            data_json.append(record_dict)
+        # Check for based on model
+        elif export_type_input == 'a':
+            ir_model_list.append(ir_model_id_input)
+
+        for ir_model in ir_model_list:
+            ### Getting Export model
+            input_model_obj = self.pool.get(ir_model.model)
+            input_model_id_list = input_model_obj.search(cr, uid, [],
+                                                         context=context)
+            # Checking for Empty data
+            if not input_model_id_list:
+                raise osv.except_osv((_('Warning')),
+                    ('There is no data for %s class' % ir_model.model))
+
+            # Select * from ...
+            input_model_list = input_model_obj.read(cr, uid, input_model_id_list,
+                                                    [], context=context)
+            ###
+
+            # Iterating export model data
+            for input_model in input_model_list:
+                record_dict = {}
+                field_dict = {}
+                record_dict['pk'] = input_model['id']
+                record_dict['model'] = ir_model.model
+
+                # Iterating model fields
+                for ir_model_field in ir_model_id_input.field_id:
+                    # Checking for existing model name in input model
+                    if ir_model_field.name in input_model:
+                        if ir_model_field.ttype == 'many2one':
+                            field_dict[ir_model_field.name] = \
+                                input_model[ir_model_field.name][0]
+                        else:
+                            field_dict[ir_model_field.name] = \
+                                input_model[ir_model_field.name]
+
+                record_dict['fields'] = field_dict
+                data_json.append(record_dict)
 
         out = base64.encodestring(json.dumps(data_json, indent=4))
 
         return self.write(cr, uid, ids, {
             'state': 'done', 'dm_export_data_wizard_data': out,
-            'name': "%s_data.json" % model_name.replace('.', '_')
+#            'name': "%s_data.json" % model_name.replace('.', '_')
+            'name': "harchi"
         }, context=context)
 
     EXPORT_TYPE = (('a', 'based on model'), ('b', 'based on module'))
